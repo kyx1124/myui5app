672de532ee390c971aec20c85355648b9866c421
xue xi <275057574@qq.com>
2025-01-18 13:08:14 +0000

    tow

--- myui5app/webapp/controller/ProductDetail.controller.js
+++ myui5app/webapp/controller/ProductDetail.controller.js
@@ -1,11 +1,9 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+    "tutorial/products/controller/BaseController",
+    "sap/m/MessageToast"
+  ], 
+  function (Controller, MessageToast) {
+    "use strict";
 
         return Controller.extend("myui5app.controller.ProductDetail", {
             onInit: function() {
@@ -18,6 +16,10 @@ sap.ui.define([
                 const oView = this.getView();
                 oView.bindElement({
                     path: "/Products(" + iProductId + ")",
+                    parameters: {
+                        expand: "Supplier,Category"
+                    },
+                    
                     events: {
                         dataRequested: function() {
                             oView.setBusy(true);
@@ -28,5 +30,28 @@ sap.ui.define([
                     }
                 });
             },
+            trimSuperfluousBytes: function (sVal) { // background info https://blogs.sap.com/2017/02/08/displaying-images-in-sapui5-received-from-the-northwind-odata-service/
+                if (typeof sVal === "string") {
+                    const sTrimmed = sVal.substring(104);
+                    return "data:image/bmp;base64," + sTrimmed;
+                }
+                return sVal;
+            },            
+            addToCart: function (oEvent) {
+                MessageToast.show("Added to cart");
+            },
+            
+            markAsFav: function (oEvent) {
+                const oButton = oEvent.getSource();
+                if (oButton.getIcon() === "sap-icon://unfavorite") {
+                    oButton.setIcon("sap-icon://favorite");
+                    MessageToast.show("Added to favorites");
+                    return;
+                }
+            
+                oButton.setIcon("sap-icon://unfavorite");
+                MessageToast.show("Removed from favorites");
+            },
+            
         });
     });
